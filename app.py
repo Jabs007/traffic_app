e961dcb2b6ac2ed71fdfb22fdceec69245dcaa01
Jabali <adamsjabali9@gmail.com>
2025-06-24 13:32:44 -0800

    commit final

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def authenticate():
         }
     }
     authenticator = stauth.Authenticate(credentials, "traffic_app", "abcdef", cookie_expiry_days=1)
-    name, authentication_status = authenticator.login("Login", location="main")
+    name, authentication_status = authenticator.login(form_name="Login", location="main")
     if authentication_status is False:
         st.error("Username/password is incorrect")
         st.stop()
